48368e0e2ca15fa090248ae63848499b816508fd
Michael Studard <mstudar1@my.westga.edu>
2021-03-01 20:49:51 -0500

    Added ClinicDBConnection class.

---
+++ Clinic/Clinic/DAL/ClinicDBConnection.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace Clinic.DAL
+{
+    /// <summary>
+    /// Class that can be used to get a SqlConnection object for the Clinic database.
+    /// </summary>
+    public static class ClinicDBConnection
+    {
+        /// <summary>
+        /// Method that can be used to get a SqlConnection object for the Clinic database.
+        /// </summary>
+        /// <returns>A SqlConnection object for the Clinic database.</returns>
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost;Initial Catalog=Clinic;" +
+                "Integrated Security=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}

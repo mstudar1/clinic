ff0e98baf7a37677bff93fc7b1d0b9b1fb5aa386
Lilit Karayan <Lilit Karayan@DESKTOP-HFVNROP>
2021-03-03 21:54:59 -0600

    Created LoginForm

--- Clinic/Clinic/View/LoginForm.cs
+++ Clinic/Clinic/View/LoginForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Clinic
 {
-    public partial class Form1 : Form
+    public partial class loginForm : Form
     {
-        public Form1()
+        public loginForm()
         {
             InitializeComponent();
         }

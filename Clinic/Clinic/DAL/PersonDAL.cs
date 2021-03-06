cb8b4952763f534e6935036f4df5acf84343d988
Michael Studard <mstudar1@my.westga.edu>
2021-03-06 17:10:15 -0500

    Added EditPerson method to PersonDAL class

--- Clinic/Clinic/DAL/PersonDAL.cs
+++ Clinic/Clinic/DAL/PersonDAL.cs
@@ -50,5 +50,113 @@ namespace Clinic.DAL
                 }
             }
         }
+
+        /// <summary>
+        /// Method that revises a record for a person in the database.
+        /// Requires that the record has not been changed since it was retrieved.
+        /// </summary>
+        /// <param name="originalPerson">The Person object that was originally retrieved.</param>
+        /// <param name="revisedPerson">A Person object with the revised values.</param>
+        /// <returns>True if the operation is successful, false otherwise.</returns>
+        public bool EditPerson(Person originalPerson, Person revisedPerson)
+        {
+            if (originalPerson == null)
+            {
+                throw new ArgumentNullException("originalPerson", "The original person cannot be null.");
+            }
+
+            if (revisedPerson == null)
+            {
+                throw new ArgumentNullException("revisedPerson", "The revised person cannot be null.");
+            }
+
+            if (originalPerson.PersonId != revisedPerson.PersonId)
+            {
+                throw new ArgumentException("The ID must be the same for both Person objects.");
+            }
+
+            string updateStatement =
+                "UPDATE Person SET " +
+                    "lastName = @RevisedLastName, " +
+                    "firstName = @RevisedFirstName, " +
+                    "dateOfBirth = @RevisedDateOfBirth, " +
+                    "ssn = @RevisedSocialSecurityNumber, " +
+                    "gender = @RevisedGender, " +
+                    "phoneNumber = @RevisedPhoneNumber, " +
+                    "addressLine1 = @RevisedAddressLine1, " +
+                    "addressLine2 = @RevisedAddressLine2, " +
+                    "city = @RevisedCity, " +
+                    "state = @RevisedState, " +
+                    "zipCode = @RevisedZipCode " +
+                "WHERE personId = @PersonId " +
+                    "AND lastName = @OriginalLastName " +
+                    "AND firstName = @OriginalFirstName " +
+                    "AND dateOfBirth = @OriginalDateOfBirth " +
+                    "AND ssn = @OriginalSocialSecurityNumber " +
+                    "AND gender = @OriginalGender " +
+                    "AND phoneNumber = @OriginalPhoneNumber " +
+                    "AND addressLine1 = @OriginalAddressLine1 " +
+                    "AND (addressLine2 = @OriginalAddressLine2 OR addressLine2 IS NULL AND @OriginalAddressLine2 IS NULL) " +
+                    "AND city = @OriginalCity " +
+                    "AND state = @OriginalState " +
+                    "AND zipCode = @OriginalZipCode";
+
+            using (SqlConnection connection = ClinicDBConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand updateCommand = new SqlCommand(updateStatement, connection))
+                {
+                    updateCommand.Parameters.AddWithValue("@PersonId", originalPerson.PersonId);
+                    updateCommand.Parameters.AddWithValue("@OriginalLastName", originalPerson.LastName);
+                    updateCommand.Parameters.AddWithValue("@OriginalFirstName", originalPerson.FirstName);
+                    updateCommand.Parameters.AddWithValue("@OriginalDateOfBirth", originalPerson.DateOfBirth);
+                    updateCommand.Parameters.AddWithValue("@OriginalSocialSecurityNumber", originalPerson.SocialSecurityNumber);
+                    updateCommand.Parameters.AddWithValue("@OriginalGender", originalPerson.Gender);
+                    updateCommand.Parameters.AddWithValue("@OriginalPhoneNumber", originalPerson.PhoneNumber);
+                    updateCommand.Parameters.AddWithValue("@OriginalAddressLine1", originalPerson.AddressLine1);
+                    updateCommand.Parameters.AddWithValue("@OriginalCity", originalPerson.City);
+                    updateCommand.Parameters.AddWithValue("@OriginalState", originalPerson.State);
+                    updateCommand.Parameters.AddWithValue("@OriginalZipCode", originalPerson.ZipCode);
+                    updateCommand.Parameters.AddWithValue("@RevisedLastName", revisedPerson.LastName);
+                    updateCommand.Parameters.AddWithValue("@RevisedFirstName", revisedPerson.FirstName);
+                    updateCommand.Parameters.AddWithValue("@RevisedDateOfBirth", revisedPerson.DateOfBirth);
+                    updateCommand.Parameters.AddWithValue("@RevisedSocialSecurityNumber", revisedPerson.SocialSecurityNumber);
+                    updateCommand.Parameters.AddWithValue("@RevisedGender", revisedPerson.Gender);
+                    updateCommand.Parameters.AddWithValue("@RevisedPhoneNumber", revisedPerson.PhoneNumber);
+                    updateCommand.Parameters.AddWithValue("@RevisedAddressLine1", revisedPerson.AddressLine1);
+                    updateCommand.Parameters.AddWithValue("@RevisedCity", revisedPerson.City);
+                    updateCommand.Parameters.AddWithValue("@RevisedState", revisedPerson.State);
+                    updateCommand.Parameters.AddWithValue("@RevisedZipCode", revisedPerson.ZipCode);
+                    
+                    if (originalPerson.AddressLine2 == default)
+                    {
+                        updateCommand.Parameters.AddWithValue("@OriginalAddressLine2", DBNull.Value);
+                    }
+                    else
+                    {
+                        updateCommand.Parameters.AddWithValue("@OriginalAddressLine2", originalPerson.AddressLine2);
+                    }
+
+                    if (revisedPerson.AddressLine2 == default)
+                    {
+                        updateCommand.Parameters.AddWithValue("@RevisedAddressLine2", DBNull.Value);
+                    }
+                    else
+                    {
+                        updateCommand.Parameters.AddWithValue("@RevisedAddressLine2", revisedPerson.AddressLine2);
+                    }
+
+                    int count = updateCommand.ExecuteNonQuery();
+                    if (count > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
     }
 }

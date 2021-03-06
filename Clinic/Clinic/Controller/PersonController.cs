a284161ee1e9da279f11e44a7ee83aea609dfe96
Michael Studard <mstudar1@my.westga.edu>
2021-03-06 17:12:20 -0500

    Added EditPerson method to PersonController class

--- Clinic/Clinic/Controller/PersonController.cs
+++ Clinic/Clinic/Controller/PersonController.cs
@@ -33,5 +33,32 @@ namespace Clinic.Controller
 
             return this.personSource.AddPerson(thePerson);
         }
+
+        /// <summary>
+        /// Method that revises a record for a person in the database.
+        /// Requires that the record has not been changed since it was retrieved.
+        /// </summary>
+        /// <param name="originalPerson">The Person object that was originally retrieved.</param>
+        /// <param name="revisedPerson">A Person object with the revised values.</param>
+        /// <returns>True if the operation is successful, false otherwise.</returns>
+        public bool EditPerson(Person originalPerson, Person revisedPerson)
+        {
+            if (originalPerson == null)
+            {
+                throw new ArgumentNullException("originalPerson", "The original person cannot be null.");
+            }
+
+            if (revisedPerson == null)
+            {
+                throw new ArgumentNullException("revisedPerson", "The revised person cannot be null.");
+            }
+
+            if (originalPerson.PersonId != revisedPerson.PersonId)
+            {
+                throw new ArgumentException("The ID must be the same for both Person objects.");
+            }
+
+            return this.personSource.EditPerson(originalPerson, revisedPerson);
+        }
     }
 }

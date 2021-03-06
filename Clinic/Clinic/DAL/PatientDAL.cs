8b890e7a308f919936a751ec2a31294ac7456a0c
Michael Studard <mstudar1@my.westga.edu>
2021-03-06 16:34:07 -0500

    Added PatientDAL class with AddPatient method

---
+++ Clinic/Clinic/DAL/PatientDAL.cs
@@ -0,0 +1,51 @@
+﻿using Clinic.Model;
+using System;
+using System.Data.SqlClient;
+
+namespace Clinic.DAL
+{
+    /// <summary>
+    /// Class for accessing patient information in the Clinic database.
+    /// </summary>
+    public class PatientDAL
+    {
+        private readonly PersonDAL thePersonDAL;
+
+        /// <summary>
+        /// Constructor for the PatientDAL class.
+        /// </summary>
+        public PatientDAL()
+        {
+            this.thePersonDAL = new PersonDAL();
+        }
+
+        /// <summary>
+        /// Method that adds the specified Patient object to the Person and Patient tables in the database.
+        /// </summary>
+        /// <param name="thePatient">The Patient object being added to the database.</param>
+        public void AddPatient(Patient thePatient)
+        {
+            if (thePatient == null)
+            {
+                throw new ArgumentNullException("thePatient", "The patient cannot be null.");
+            }
+
+            int personId = this.thePersonDAL.AddPerson(thePatient);
+
+            string insertStatement =
+                "INSERT Patient (personId, patientId) " +
+                "VALUES (@PersonId, @PatientId)";
+
+            using (SqlConnection connection = ClinicDBConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand insertCommand = new SqlCommand(insertStatement, connection))
+                {
+                    insertCommand.Parameters.AddWithValue("@PersonId", personId);
+                    insertCommand.Parameters.AddWithValue("@PatientId", thePatient.PatientId);
+                    insertCommand.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}

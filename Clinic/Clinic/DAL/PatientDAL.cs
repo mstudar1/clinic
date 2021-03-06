4cac8fe01f66c0e1312b9722d94304216ae5428b
Michael Studard <mstudar1@my.westga.edu>
2021-03-06 17:24:22 -0500

    Added EditPatient method to PatientDAL class

--- Clinic/Clinic/DAL/PatientDAL.cs
+++ Clinic/Clinic/DAL/PatientDAL.cs
@@ -47,5 +47,60 @@ namespace Clinic.DAL
                 }
             }
         }
+
+        /// <summary>
+        /// Method that revises a record for a patient in the database.
+        /// Edits the Person and Patient tables.
+        /// Requires that the record has not been changed since it was retrieved.
+        /// </summary>
+        /// <param name="originalPatient">The Patient object that was originally retrieved.</param>
+        /// <param name="revisedPatient">A Patient object with the revised values.</param>
+        /// <returns>True if the operation is successful, false otherwise.</returns>
+        public bool EditPatient(Patient originalPatient, Patient revisedPatient)
+        {
+            if (originalPatient == null)
+            {
+                throw new ArgumentNullException("originalPatient", "The original patient cannot be null.");
+            }
+
+            if (revisedPatient == null)
+            {
+                throw new ArgumentNullException("revisedPatient", "The revised patient cannot be null.");
+            }
+
+            if (originalPatient.PersonId != revisedPatient.PersonId)
+            {
+                throw new ArgumentException("The ID must be the same for both Patient objects.");
+            }
+
+            this.thePersonDAL.EditPerson(originalPatient, revisedPatient);
+
+            string updateStatement =
+                "UPDATE Patient SET " +
+                    "patientId = @RevisedPatientId, " +
+                "WHERE personId = @PersonId " +
+                    "AND patientId = @OriginalPatientId";
+
+            using (SqlConnection connection = ClinicDBConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand updateCommand = new SqlCommand(updateStatement, connection))
+                {
+                    updateCommand.Parameters.AddWithValue("@PersonId", originalPatient.PersonId);
+                    updateCommand.Parameters.AddWithValue("@OriginalPatientId", originalPatient.PatientId);
+                    updateCommand.Parameters.AddWithValue("@RevisedPatientId", revisedPatient.PatientId);
+
+                    int count = updateCommand.ExecuteNonQuery();
+                    if (count > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
     }
 }

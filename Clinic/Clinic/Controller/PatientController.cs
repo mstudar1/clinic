f71c4421ce52e93f49cf8c089948058c32bcce22
Michael Studard <mstudar1@my.westga.edu>
2021-03-06 16:37:54 -0500

    Added PatientController class with AddPatient method

---
+++ Clinic/Clinic/Controller/PatientController.cs
@@ -0,0 +1,36 @@
+﻿using Clinic.DAL;
+using Clinic.Model;
+using System;
+
+namespace Clinic.Controller
+{
+    /// <summary>
+    /// Class serving as a controller for patients.
+    /// </summary>
+    public class PatientController
+    {
+        private readonly PatientDAL patientSource;
+
+        /// <summary>
+        /// Constructor for the PatientController class.
+        /// </summary>
+        public PatientController()
+        {
+            this.patientSource = new PatientDAL();
+        }
+
+        /// <summary>
+        /// Method that adds the specified Patient object to the Person and Patient tables in the database.
+        /// </summary>
+        /// <param name="thePatient">The Patient object being added to the database.</param>
+        public void AddPatient(Patient thePatient)
+        {
+            if (thePatient == null)
+            {
+                throw new ArgumentNullException("thePatient", "The patient cannot be null.");
+            }
+
+            this.patientSource.AddPatient(thePatient);
+        }
+    }
+}

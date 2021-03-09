7b4d43ef23aeb621e1bc2c7dde4339751254c7ec
Michael Studard <mstudar1@my.westga.edu>
2021-03-09 17:22:46 -0500

    Added AppointmentController class with AddAppointment method

---
+++ Clinic/Clinic/Controller/AppointmentController.cs
@@ -0,0 +1,36 @@
+﻿using Clinic.DAL;
+using Clinic.Model;
+using System;
+
+namespace Clinic.Controller
+{
+    /// <summary>
+    /// Class serving as a controller for appointments.
+    /// </summary>
+    public class AppointmentController
+    {
+        private readonly AppointmentDAL appointmentSource;
+
+        /// <summary>
+        /// Constructor for the AppointmentController class.
+        /// </summary>
+        public AppointmentController()
+        {
+            this.appointmentSource = new AppointmentDAL();
+        }
+
+        /// <summary>
+        /// Method that adds the specified Appointment object to the database.
+        /// </summary>
+        /// <param name="theAppointment">The Appointment object being added to the database.</param>
+        public void AddAppointment(Appointment theAppointment)
+        {
+            if (theAppointment == null)
+            {
+                throw new ArgumentNullException("theAppointment", "The appointment cannot be null.");
+            }
+
+            this.appointmentSource.AddAppointment(theAppointment);
+        }
+    }
+}

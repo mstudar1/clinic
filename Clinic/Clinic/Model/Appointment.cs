652adc27c50e3563097edd33a74390128cba2bef
Michael Studard <mstudar1@my.westga.edu>
2021-03-09 17:10:16 -0500

    Added Appointment model class

---
+++ Clinic/Clinic/Model/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Clinic.Model
+{
+    /// <summary>
+    /// Class representing an appointment.
+    /// </summary>
+    public class Appointment
+    {
+        public int PatientId { get; set; }
+
+        public DateTime DateAndTime { get; set; }
+
+        public int DoctorId { get; set; }
+
+        public string ReasonForVisit { get; set; }
+    }
+}

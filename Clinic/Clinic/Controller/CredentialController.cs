491dad8fc49148559caf90b28489dd49c837f630
Michael Studard <mstudar1@my.westga.edu>
2021-03-15 17:50:35 -0400

    Added GetUser method to CredentialController class

--- Clinic/Clinic/Controller/CredentialController.cs
+++ Clinic/Clinic/Controller/CredentialController.cs
@@ -1,4 +1,5 @@
 ﻿using Clinic.DAL;
+using Clinic.Model;
 using System;
 
 namespace Clinic.Controller
@@ -37,6 +38,25 @@ namespace Clinic.Controller
             }
 
             return this.credentialSource.CredentialsAreValid(username, password);
+        }
+
+        /// <summary>
+        /// Method that returns an object that is a subtype of the Person class.
+        /// The returned object represents the user with the specified username.
+        /// If the specified user has a role of "Nurse", then an object of the Nurse type is returned.
+        /// Similarly, if the specified user has a role of "Administrator", then an object of the Administrator type is returned.
+        /// If the specified user does not have rights to log in to the system, then an ArgumentException is thrown.
+        /// </summary>
+        /// <param name="username">The username of the user that is attempting to log in to the system.</param>
+        /// <returns>An object that is a subtype of the Person class which represents the user with the specified username.</returns>
+        public Person GetUser(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentNullException("username", "The username cannot be null or empty.");
+            }
+
+            return this.credentialSource.GetUser(username);
         }
     }
 }

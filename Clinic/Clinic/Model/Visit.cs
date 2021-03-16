ed42cb44c5391a219fef60ac93585678cfa7c7cf
Michael Studard <mstudar1@my.westga.edu>
2021-03-16 17:25:28 -0400

    Added Visit model class

---
+++ Clinic/Clinic/Model/Visit.cs
@@ -0,0 +1,24 @@
+﻿namespace Clinic.Model
+{
+    /// <summary>
+    /// Class representing a visit.
+    /// </summary>
+    public class Visit
+    {
+        public Appointment AssociatedAppointment { get; set; }
+
+        public double Weight { get; set; }
+
+        public int SystolicBloodPressure { get; set; }
+        
+        public int DiastolicBloodPressure { get; set; }
+
+        public double BodyTemperature { get; set; }
+
+        public int Pulse { get; set; }
+
+        public string Symptoms { get; set; }
+
+        public int NurseId { get; set; }
+    }
+}

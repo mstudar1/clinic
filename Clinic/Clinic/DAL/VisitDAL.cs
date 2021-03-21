7767201c0056b9f40abaf8de699d37298fcd4f8a
Michael Studard <mstudar1@my.westga.edu>
2021-03-21 11:30:33 -0400

    Added FindVisits method to VisitDAL class

--- Clinic/Clinic/DAL/VisitDAL.cs
+++ Clinic/Clinic/DAL/VisitDAL.cs
@@ -1,5 +1,6 @@
 ﻿using Clinic.Model;
 using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace Clinic.DAL
@@ -40,6 +41,62 @@ namespace Clinic.DAL
                     insertCommand.ExecuteNonQuery();
                 }
             }
+        }
+
+        /// <summary>
+        /// Method that finds all of the visits for the specified patient.
+        /// </summary>
+        /// <param name="patientId">The ID of the patient.</param>
+        /// <returns>A list of Visit objects associated with the specified patient.</returns>
+        public List<Visit> FindVisits(int patientId)
+        {
+            if (patientId < 0)
+            {
+                throw new ArgumentException("The patient ID cannot be negative.", "patientId");
+            }
+
+            List<Visit> visitList = new List<Visit>();
+
+            string selectStatement =
+                "SELECT v.appointmentId, v.nurseId, v.weight, v.pulse, v.systolicBloodPressure, v.diastolicBloodPressure, v.bodyTemperature, v.symptoms " +
+                "FROM Visit v " +
+                "JOIN Appointment a ON v.appointmentId = a.appointmentId " +
+                "WHERE a.patientId = @PatientId " +
+                "ORDER BY a.dateAndTime ASC";
+
+            using (SqlConnection connection = ClinicDBConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.AddWithValue("@PatientId", patientId);
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        int appointmentIdOrdinal = reader.GetOrdinal("appointmentId");
+                        int nurseIdOrdinal = reader.GetOrdinal("nurseId");
+                        int weightOrdinal = reader.GetOrdinal("weight");
+                        int pulseOrdinal = reader.GetOrdinal("pulse");
+                        int systolicBloodPressureOrdinal = reader.GetOrdinal("systolicBloodPressure");
+                        int diastolicBloodPressureOrdinal = reader.GetOrdinal("diastolicBloodPressure");
+                        int bodyTemperatureOrdinal = reader.GetOrdinal("bodyTemperature");
+                        int symptomsOrdinal = reader.GetOrdinal("symptoms");
+                        while (reader.Read())
+                        {
+                            Visit theVisit = new Visit();
+                            if (!reader.IsDBNull(appointmentIdOrdinal)) { theVisit.AppointmentId = reader.GetInt32(appointmentIdOrdinal); }
+                            if (!reader.IsDBNull(nurseIdOrdinal)) { theVisit.NurseId = reader.GetInt32(nurseIdOrdinal); }
+                            if (!reader.IsDBNull(weightOrdinal)) { theVisit.Weight = reader.GetDouble(weightOrdinal); }
+                            if (!reader.IsDBNull(pulseOrdinal)) { theVisit.Pulse = reader.GetInt32(pulseOrdinal); }
+                            if (!reader.IsDBNull(systolicBloodPressureOrdinal)) { theVisit.SystolicBloodPressure = reader.GetInt32(systolicBloodPressureOrdinal); }
+                            if (!reader.IsDBNull(diastolicBloodPressureOrdinal)) { theVisit.DiastolicBloodPressure = reader.GetInt32(diastolicBloodPressureOrdinal); }
+                            if (!reader.IsDBNull(bodyTemperatureOrdinal)) { theVisit.BodyTemperature = reader.GetDouble(bodyTemperatureOrdinal); }
+                            if (!reader.IsDBNull(symptomsOrdinal)) { theVisit.Symptoms = reader.GetString(symptomsOrdinal); }
+                            visitList.Add(theVisit);
+                        }
+                    }
+                }
+            }
+            return visitList;
         }
     }
 }

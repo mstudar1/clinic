3de079e4418e9b30d3fa6275dbc196d59e3a113e
Michael Studard <mstudar1@my.westga.edu>
2021-03-21 11:32:18 -0400

    Added FindVisits method to VisitController class

--- Clinic/Clinic/Controller/VisitController.cs
+++ Clinic/Clinic/Controller/VisitController.cs
@@ -1,6 +1,7 @@
 ﻿using Clinic.DAL;
 using Clinic.Model;
 using System;
+using System.Collections.Generic;
 
 namespace Clinic.Controller
 {
@@ -31,6 +32,21 @@ namespace Clinic.Controller
             }
 
             this.visitSource.AddVisit(theVisit);
+        }
+
+        /// <summary>
+        /// Method that finds all of the visits for the specified patient.
+        /// </summary>
+        /// <param name="patientId">The ID of the patient.</param>
+        /// <returns>A list of Visit objects associated with the specified patient.</returns>
+        public List<Visit> FindVisits(int patientId)
+        {
+            if (patientId < 0)
+            {
+                throw new ArgumentException("The patient ID cannot be negative.", "patientId");
+            }
+
+            return this.visitSource.FindVisits(patientId);
         }
     }
 }

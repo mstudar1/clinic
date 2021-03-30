b2d682175811556c3e2b193b5bbf51dd0db29fdd
J. Allen Burton <jburton6@my.westga.edu>
2021-03-30 09:19:38 -0400

    Created DAL and controller for nurse with needed methods for adding nurses to DB

---
+++ Clinic/Clinic/Controller/NurseController.cs
@@ -0,0 +1,34 @@
+﻿using Clinic.DAL;
+using Clinic.Model;
+using System;
+
+namespace Clinic.Controller
+{
+    class NurseController
+    {
+        private readonly NurseDAL nurseSource;
+
+        /// <summary>
+        /// Constructor for the PatientController class.
+        /// </summary>
+        public NurseController()
+        {
+            this.nurseSource = new NurseDAL();
+        }
+
+        /// <summary>
+        /// Method that adds the specified Patient object to the Person and Patient tables in the database.
+        /// </summary>
+        /// <param name="thePatient">The Patient object being added to the database.</param>
+        public void AddPatient(Nurse theNurse)
+        {
+            if (theNurse == null)
+            {
+                throw new ArgumentNullException("theNurse", "The nurse cannot be null.");
+            }
+
+            this.nurseSource.AddPatient(theNurse);
+        }
+    }
+}
+
---
+++ Clinic/Clinic/DAL/NurseDAL.cs
@@ -0,0 +1,52 @@
+﻿using Clinic.Model;
+using System;
+using System.Data.SqlClient;
+
+namespace Clinic.DAL
+{
+    class NurseDAL
+    {
+        private readonly PersonDAL thePersonDAL;
+
+        /// <summary>
+        /// Constructor for the PatientDAL class.
+        /// </summary>
+        public NurseDAL()
+        {
+            this.thePersonDAL = new PersonDAL();
+        }
+
+        /// <summary>
+        /// Method that adds the specified Patient object to the Person and Patient tables in the database.
+        /// </summary>
+        /// <param name="thePatient">The Patient object being added to the database.</param>
+        public void AddPatient(Nurse theNurse)
+        {
+            if (theNurse == null)
+            {
+                throw new ArgumentNullException("theNurse", "The nurse cannot be null.");
+            }
+
+            if (theNurse.NurseId != default)
+            {
+                throw new ArgumentException("The Nurse object being passed in cannot have an ID, because one will be assigned by the database.");
+            }
+
+            int personId = this.thePersonDAL.AddPerson(theNurse);
+
+            string insertStatement =
+                "INSERT Nurse (personId) " +
+                "VALUES (@PersonId)";
+
+            using (SqlConnection connection = ClinicDBConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand insertCommand = new SqlCommand(insertStatement, connection))
+                {
+                    insertCommand.Parameters.AddWithValue("@PersonId", personId);
+                    insertCommand.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}

ae526049563d1d56e8cd1de0a1a49db239a91eeb
Michael Studard <mstudar1@my.westga.edu>
2021-03-31 17:28:43 -0400

    Added date picker to visit user control

--- Clinic/Clinic/UserControls/VisitUserControl.cs
+++ Clinic/Clinic/UserControls/VisitUserControl.cs
@@ -16,5 +16,15 @@ namespace Clinic.UserControls
         {
             InitializeComponent();
         }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

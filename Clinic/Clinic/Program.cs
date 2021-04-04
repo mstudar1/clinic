8a953297293c4352e38a089fa16dc2909d65c37a
Michael Studard <mstudar1@my.westga.edu>
2021-04-03 22:09:47 -0400

    Modified Program class to launch with login form instead of add visit form

--- Clinic/Clinic/Program.cs
+++ Clinic/Clinic/Program.cs
@@ -1,6 +1,4 @@
-﻿using Clinic.Model;
-using Clinic.View;
-using System;
+﻿using System;
 using System.Windows.Forms;
 
 namespace Clinic
@@ -15,19 +13,7 @@ namespace Clinic
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new LoginForm());
-
-            // the temporary block of code shown below is for launching the add visit form directly
-            Appointment testAppointment = new Appointment
-            {
-                AppointmentId = 1,
-                PatientFirstName = "Tom",
-                PatientLastName = "Cat",
-                DoctorFirstName = "Bugs",
-                DoctorLastName = "Bunny",
-                StartDateTime = new DateTime(2021, 4, 15, 8, 30, 0)
-            };
-            Application.Run(new AddVisitForm(testAppointment));
+            Application.Run(new LoginForm());
         }
     }
 }

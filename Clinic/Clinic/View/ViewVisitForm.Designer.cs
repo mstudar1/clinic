2e74650971f9aa0e3d2356a0eff56174394e2b76
Michael Studard <mstudar1@my.westga.edu>
2021-04-05 19:20:57 -0400

    Modified properties of ViewVisitForm class

--- Clinic/Clinic/View/ViewVisitForm.Designer.cs
+++ Clinic/Clinic/View/ViewVisitForm.Designer.cs
@@ -29,10 +29,20 @@ namespace Clinic.View
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ViewVisitForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ViewVisitForm";
+            this.ClientSize = new System.Drawing.Size(708, 747);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ViewVisitForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "View Visit Details";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

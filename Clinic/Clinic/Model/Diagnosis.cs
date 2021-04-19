7068706d6b145ac47e1d3b1a375a618dfb35944c
Michael Studard <mstudar1@my.westga.edu>
2021-04-19 13:53:23 -0400

    Added Diagnosis model class

---
+++ Clinic/Clinic/Model/Diagnosis.cs
@@ -0,0 +1,14 @@
+﻿namespace Clinic.Model
+{
+    /// <summary>
+    /// Class representing a diagnosis.
+    /// </summary>
+    public class Diagnosis
+    {
+        public int AppointmentId { get; set; }
+
+        public string DiagnosisName { get; set; }
+
+        public bool IsFinal { get; set; }
+    }
+}

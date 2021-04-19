56919b8cefcd599e747e9e56291ea26be0fca4eb
Michael Studard <mstudar1@my.westga.edu>
2021-04-19 17:30:56 -0400

    Added LabTestDAL class with GetAllLabTests method

---
+++ Clinic/Clinic/DAL/LabTestDAL.cs
@@ -0,0 +1,47 @@
+﻿using Clinic.Model;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Clinic.DAL
+{
+    /// <summary>
+    /// Class for accessing lab test information in the Clinic database.
+    /// </summary>
+    public class LabTestDAL
+    {
+        /// <summary>
+        /// Method that returns a list of all the LabTest objects in the database.
+        /// </summary>
+        /// <returns>A list of all the LabTest objects.</returns>
+        public List<LabTest> GetAllLabTests()
+        {
+            List<LabTest> labTests = new List<LabTest>();
+
+            string selectStatement =
+                "SELECT testCode, name " +
+                "FROM LabTest " +
+                "ORDER BY name";
+
+            using (SqlConnection connection = ClinicDBConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        int testCodeOrdinal = reader.GetOrdinal("testCode");
+                        int nameOrdinal = reader.GetOrdinal("name");
+                        while (reader.Read())
+                        {
+                            LabTest labTest = new LabTest();
+                            if (!reader.IsDBNull(testCodeOrdinal)) { labTest.TestCode = reader.GetInt32(testCodeOrdinal); }
+                            if (!reader.IsDBNull(nameOrdinal)) { labTest.Name = reader.GetString(nameOrdinal); }
+                            labTests.Add(labTest);
+                        }
+                    }
+                }
+            }
+            return labTests;
+        }
+    }
+}

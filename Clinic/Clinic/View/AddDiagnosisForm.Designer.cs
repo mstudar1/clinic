53ee7a85fd055523645dc6698b2c1a7ebbe462bc
J. Allen Burton <jburton6@my.westga.edu>
2021-04-19 13:54:22 -0400

    Added form elements except radios and alert message label

--- Clinic/Clinic/View/AddDiagnosisForm.Designer.cs
+++ Clinic/Clinic/View/AddDiagnosisForm.Designer.cs
@@ -30,6 +30,13 @@ namespace Clinic.View
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.diagnosisLabel = new System.Windows.Forms.Label();
+            this.finalLabel = new System.Windows.Forms.Label();
+            this.OkayButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -39,6 +46,12 @@ namespace Clinic.View
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.titleLabel, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.diagnosisLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.finalLabel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.OkayButton, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.cancelButton, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 1);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(13, 13);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
@@ -48,6 +61,69 @@ namespace Clinic.View
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(511, 164);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.titleLabel.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.titleLabel, 2);
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.titleLabel.Location = new System.Drawing.Point(130, 10);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(248, 29);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Record Diagnosis";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // diagnosisLabel
+            // 
+            this.diagnosisLabel.AutoSize = true;
+            this.diagnosisLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diagnosisLabel.Location = new System.Drawing.Point(3, 50);
+            this.diagnosisLabel.Name = "diagnosisLabel";
+            this.diagnosisLabel.Size = new System.Drawing.Size(74, 17);
+            this.diagnosisLabel.TabIndex = 1;
+            this.diagnosisLabel.Text = "Diagnosis:";
+            // 
+            // finalLabel
+            // 
+            this.finalLabel.AutoSize = true;
+            this.finalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.finalLabel.Location = new System.Drawing.Point(3, 85);
+            this.finalLabel.Name = "finalLabel";
+            this.finalLabel.Size = new System.Drawing.Size(46, 17);
+            this.finalLabel.TabIndex = 2;
+            this.finalLabel.Text = "Final?";
+            // 
+            // OkayButton
+            // 
+            this.OkayButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OkayButton.Location = new System.Drawing.Point(130, 123);
+            this.OkayButton.Name = "OkayButton";
+            this.OkayButton.Size = new System.Drawing.Size(108, 29);
+            this.OkayButton.TabIndex = 3;
+            this.OkayButton.Text = "Okay";
+            this.OkayButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.Location = new System.Drawing.Point(257, 123);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(108, 29);
+            this.cancelButton.TabIndex = 4;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 3);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(130, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 5;
             // 
             // AddDiagnosisForm
             // 
@@ -61,6 +137,8 @@ namespace Clinic.View
             this.Name = "AddDiagnosisForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Add Diagnosis";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -68,5 +146,11 @@ namespace Clinic.View
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label diagnosisLabel;
+        private System.Windows.Forms.Label finalLabel;
+        private System.Windows.Forms.Button OkayButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }

22ff62ccd19b05147b985bb040a5865f6fa827dd
Michael Studard <mstudar1@my.westga.edu>
2021-04-18 20:38:10 -0400

    Added LabTestController with dummy GetAllLabTests method

---
+++ Clinic/Clinic/Controller/LabTestController.cs
@@ -0,0 +1,43 @@
+﻿using Clinic.Model;
+using System.Collections.Generic;
+
+namespace Clinic.Controller
+{
+    /// <summary>
+    /// Class serving as a controller for lab tests.
+    /// </summary>
+    public class LabTestController
+    {
+        /// <summary>
+        /// Method that returns a list of all the LabTest objects in the database.
+        /// </summary>
+        /// <returns>A list of all the LabTest objects.</returns>
+        public List<LabTest> GetAllLabTests()
+        {
+            List<LabTest> dummyList = new List<LabTest>();
+
+            LabTest firstLabTest = new LabTest
+            {
+                TestCode = 1,
+                Name = "X-ray"
+            };
+            dummyList.Add(firstLabTest);
+
+            LabTest secondLabTest = new LabTest
+            {
+                TestCode = 2,
+                Name = "MRI"
+            };
+            dummyList.Add(secondLabTest);
+
+            LabTest thirdLabTest = new LabTest
+            {
+                TestCode = 3,
+                Name = "Strep Test"
+            };
+            dummyList.Add(thirdLabTest);
+
+            return dummyList;
+        }
+    }
+}

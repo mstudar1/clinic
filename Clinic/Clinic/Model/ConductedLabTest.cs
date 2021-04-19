bb2ea78ea77d9c3685ee6d23bb91c251b9d37801
Michael Studard <mstudar1@my.westga.edu>
2021-04-18 20:30:01 -0400

    Added ConductedLabTest model class

---
+++ Clinic/Clinic/Model/ConductedLabTest.cs
@@ -0,0 +1,21 @@
+﻿using Clinic.DAL;
+using System;
+
+namespace Clinic.Model
+{
+    /// <summary>
+    /// Class representing a conducted lab test.
+    /// </summary>
+    public class ConductedLabTest
+    {
+        public int AppointmentId { get; set; }
+
+        public LabTest LabTest { get; set; }
+
+        public DateTime DatePerformed { get; set; }
+
+        public string Results { get; set; }
+
+        public bool IsNormal { get; set; }
+    }
+}

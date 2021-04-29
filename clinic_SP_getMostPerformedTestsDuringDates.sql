472d362e116b1def21a71c174abecfd1d5c998c7
J. Allen Burton <jburton6@my.westga.edu>
2021-04-29 14:43:09 -0400

    Saving progress - still errors in SP

--- clinic_SP_getMostPerformedTestsDuringDates.sql
+++ clinic_SP_getMostPerformedTestsDuringDates.sql
@@ -12,7 +12,7 @@ CREATE PROCEDURE getMostPerformedTestsDuringDates(
 AS
 BEGIN
 	/* Verify that dates are nto in the future */
-	IF @startDate > CURDATE()
+	IF @startDate > GETDATE()
 	BEGIN
 		RAISERROR('Invalid parameter: Start date cannot be in the future', 12, 1)
 		RETURN
@@ -32,12 +32,15 @@ BEGIN
 		RETURN
 	END
 
+	/* Declare variable to hold total test count */
+	DECLARE @totalTestCount INT;
+
 	/* Get and store count of total tests performed */
     SET @totalTestCount = (
 		SELECT COUNT(*) AS totalTests
 		FROM ConductedLabTest
-		WHERE resultsDate > @startdate
-			AND resultsDate < @enddate
+		WHERE datePerformed > @startdate
+			AND datePerformed < @enddate
 	);
     
     /* Build result */
@@ -46,10 +49,10 @@ BEGIN
 		lt.name, 
 		COUNT(*) AS testCount,  
 		@totalTestCount AS totalTests,
-		CONCAT(ROUND((100 * COUNT(*) / @totalTestCount),1), "%") AS percent,   
+		CONCAT(ROUND((100 * COUNT(*) / @totalTestCount),1), '%') AS percentOfTests,   
 		COUNT(CASE WHEN clt.isNormal = 1 THEN 1 ELSE NULL END) AS normalCount,
-		COUNT(IF(clt.isNormal = 0, 1, NULL)) AS abnormalCount,
-		COUNT(dbo.isInAgeRange(clt.appointmentId, clt.testDate, 18, 29)) AS tests18to29,
+		COUNT(CASE WHEN clt.isNormal = 0 THEN 1 ELSE NULL END) AS abnormalCount,
+		COUNT(CASE WHEN dbo.isInAgeRange(clt.appointmentId, clt.testDate, 18, 29) = 1 THEN 1 ELSE NULL END) AS tests18to29,
 		COUNT(dbo.isInAgeRange(clt.appointmentId, clt.testDate, 30, 39)) AS tests30to39		
 	FROM
 		ConductedLabTest clt
@@ -57,9 +60,9 @@ BEGIN
 		JOIN Appointment a ON clt.appointmentId = a.appointmentId
 		JOIN Patient pat ON a.patientId = pat.patientId
 		JOIN Person per ON pat.personId = per.personId
-	WHERE resultsDate > @startdate
-		AND resultsDate < @enddate
-	GROUP BY lo.testCode
+	WHERE datePerformed > @startdate
+		AND datePerformed < @enddate
+	GROUP BY clt.testCode
 	HAVING COUNT(*) > 1
     ORDER BY COUNT(*) DESC, clt.testCode DESC
 	;

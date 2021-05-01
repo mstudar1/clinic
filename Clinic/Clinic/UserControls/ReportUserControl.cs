1e51b82581dc5114cc289ec4757721a3e831f4cc
J. Allen Burton <jburton6@my.westga.edu>
2021-05-01 13:32:40 -0400

    Added date validation.

--- Clinic/Clinic/UserControls/ReportUserControl.cs
+++ Clinic/Clinic/UserControls/ReportUserControl.cs
@@ -26,14 +26,26 @@ namespace Clinic.UserControls
         {
             DateTime start = this.startDateTimePicker.Value;
             DateTime end = this.endDateTimePicker.Value;
+            if (this.DatesAreValid(start, end)) {
+                ReportParameter[] parameters = new ReportParameter[2];
+                parameters[0] = new ReportParameter("startDate", start.ToString("D"));
+                parameters[1] = new ReportParameter("endDate", end.ToString("D"));
+                this.reportViewer.LocalReport.SetParameters(parameters);
+                this.getMostPerformedTestsDuringDatesTableAdapter.Fill(_labTestReportDataSet.getMostPerformedTestsDuringDates, start, end);
+                this.reportViewer.RefreshReport();
+            }
+            else
+            {
+                string boxTitle = "Date Error";
+                string boxMessage = "The end date must be later than the start date.";              
+                MessageBox.Show(boxMessage, boxTitle);
+            }
+            
+        }
 
-            ReportParameter[] parameters = new ReportParameter[2];
-            parameters[0] = new ReportParameter("startDate", start.ToString("D"));
-            parameters[1] = new ReportParameter("endDate", end.ToString("D"));
-            this.reportViewer.LocalReport.SetParameters(parameters);
-
-            this.getMostPerformedTestsDuringDatesTableAdapter.Fill(_labTestReportDataSet.getMostPerformedTestsDuringDates, start, end);
-            this.reportViewer.RefreshReport();
+        private bool DatesAreValid(DateTime startDate, DateTime endDate)
+        {
+            return (DateTime.Compare(startDate, endDate) < 0);
         }
     }
 }
